0212da17e9d478f033c59f381b1af2416fe229aa
talal-5 <164428148+talal-5@users.noreply.github.com>
2025-06-12 15:48:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+"use strict";
+const text = document.querySelector("#text");
+const bird1 = document.querySelector("#bird1");
+const bird2 = document.querySelector("#bird2");
+const btn = document.querySelector("#btn");
+const rocks = document.querySelector("#rocks");
+const forest = document.querySelector("#forest");
+const water = document.querySelector("#water");
+const header = document.querySelector("#header");
+window.addEventListener("scroll", () => {
+  let value = window.scrollY;
+  text.style.top = 50 + value * -0.1 + "%";
+  bird2.style.top = value * -1.5 + "px";
+  bird2.style.left = value * 2 + "px";
+  bird1.style.left = value * -1.5 + "px";
+  bird1.style.left = value * -5 + "px";
+  btn.style.marginTop = value * 1.5 + "px";
+  rocks.style.top = value * -0.12 + "px";
+  forest.style.top = value * 0.25 + "px";
+  header.style.top = value * 0.5 + "px";
+});
